54051b370eb1ddbabc151bcc82f8991451a1ab6c
Chase Zimmerman <chaseklvk@icloud.com>
2022-10-28 19:28:06 -0700

    algorithm to construct inner ring

--- board/space.py
+++ board/space.py
@@ -1,16 +1,65 @@
 """
 Class representing a single node in the graph
 """
+
+# TODO: How do we manage keeping track of tiles associated with each space?
 class Space:
-	def __init__(self, space_id):
+	def __init__(self, space_id, origin_tile):
 		self._space_id = space_id
 		self._neighbors = []
+		self._current_tile = origin_tile
 	
 	def add_neighbor(self, node):
 		self._neighbors.append(node)
 	
+	def reduce_edges(self):
+		'''
+		Check for neighbor list of size 4, if found reduce to 3 by combining
+		the two neighbors with the least edges
+		'''
+		if self.num_neighbors() <= 3: return
+
+		# Find the two neighbors with the least edges
+		two_edge_neighbors = []
+		for neighbor in self._neighbors:
+			if neighbor.num_neighbors() == 2: 
+				two_edge_neighbors.append(neighbor)
+
+		# Combine two-edge neighbors into a single node
+		node_1 = two_edge_neighbors.pop()
+		node_2 = two_edge_neighbors.pop()
+
+		for osn in node_2.neighbors():
+			if osn != node_1 and (osn not in self.neighbors()):
+				node_1.add_neighbor(osn)
+				osn.add_neighbor(node_1)
+		
+		node_2.isolate()
+
+		node_2.current_tile().set_space(node_1, node_2.current_tile().ring().index(node_2))
+
+		# Close side on node 1's tile
+		node_1.current_tile().close_side()
+		node_2.current_tile().close_side()
+
+	
+	def isolate(self):
+		# Removes space from all neighbors' lists
+		# Effectively isolating it from the graph
+		for neighbor in self.neighbors():
+			neighbor.neighbors().remove(self)
+
+	def current_tile(self):
+		return self._current_tile
+	
+	def set_tile(self, tile):
+		self._current_tile = tile
+
 	def neighbors(self):
 		return self._neighbors
+
+	def num_neighbors(self):
+		return len(self._neighbors)
 
 	def space_id(self):
 		return self._space_id
--- board/tile.py
+++ board/tile.py
@@ -15,11 +15,11 @@ class Tile:
 		# Construct the tile ring
 		self._ring = []
 		
-		start_space = Space(f"{tile_id}{0}")
+		start_space = Space(f"{tile_id}{0}", self)
 		self._ring.append(start_space)
 		current_space = start_space
 		for i in range(5):
-			next_space = Space(f"{tile_id}{i + 1}")
+			next_space = Space(f"{tile_id}{i + 1}", self)
 			self._ring.append(next_space)
 
 			current_space.add_neighbor(next_space)
@@ -38,55 +38,54 @@ class Tile:
 		return self._ring
 	
 	def next_open_side(self):
-		return (self._closed_sides % 6, (self._closed_sides + 1) % 6)
-	
-	def space_at_index(self, i):
-		return self._ring[i]
+		start_index = self._closed_sides % 6 
+		end_index = (self._closed_sides + 1) % 6
+
+		return (start_index, self._ring[start_index]), (end_index, self._ring[end_index])
 
 	def set_space(self, space, i):
 		self._ring[i] = space
 
-	def isolate_space(self, space_to_remove):
-		# Removes space from all neighbors' lists
-		# Effectively isolating it from the graph
-		for neighbor in space_to_remove.neighbors():
-			neighbor.neighbors().remove(space_to_remove)
-
 	def connect_tile(self, tile):
 		# Select the next open side
-		this_open_side = self.next_open_side()
-		other_open_side = tile.next_open_side()
-
+		(this_start_index, this_start), (this_end_index, this_end) = self.next_open_side()
+		(other_start_index, other_start), (other_end_index, other_end) = tile.next_open_side()
+		
 		# Connect the sides by consolidating the spaces
 		# and drawing the right edges... done by:
 		# 1. Set the neighbors of the other side to be the neighbors of this side (except the other spaces themselves)
 		# 2. Set the spaces of this side to the correct index in the other side's array
+		# 3. Check for nodes with 4 edges and consolidate them
 
 		# 1
-		other_start = tile.space_at_index(other_open_side[0])
-		other_end = tile.space_at_index(other_open_side[1])
-
 		for osn in other_start.neighbors():
-			if osn != other_end:
-				self.space_at_index(this_open_side[0]).add_neighbor(osn)
-				osn.add_neighbor(self.space_at_index(this_open_side[0]))
+			if osn != other_end and (osn not in this_start.neighbors()):
+				this_start.add_neighbor(osn)
+				osn.add_neighbor(this_start)
 		
 		for osn in other_end.neighbors():
-			if osn != other_start:
-				self.space_at_index(this_open_side[1]).add_neighbor(osn)
-				osn.add_neighbor(self.space_at_index(this_open_side[1]))
+			if osn != other_start and (osn not in this_end.neighbors()):
+				this_end.add_neighbor(osn)
+				osn.add_neighbor(this_end)
 
 		# Isolate spaces
-		tile.isolate_space(other_start)
-		tile.isolate_space(other_end)
+		other_start.isolate()
+		other_end.isolate()
 
 		# 2
-		tile.set_space(self.space_at_index(this_open_side[0]), other_open_side[0])
-		tile.set_space(self.space_at_index(this_open_side[1]), other_open_side[1])
+		tile.set_space(this_start, other_start_index)
+		tile.set_space(this_end, other_end_index)
+
+		# 3
+		this_start.reduce_edges()
+		this_end.reduce_edges()
 
 		# We've closed a side
-		self._closed_sides += 1
-		tile._closed_sides += 1
+		self.close_side()
+		tile.close_side()
 	
+	def close_side(self):
+		self._closed_sides += 1
+
 	def __repr__(self):
 		return self.tile_id()
